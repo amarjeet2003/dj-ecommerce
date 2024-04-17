81dcb32681cc7699c0a8a9d2f636a38468cc348f
Amarjeet Singh <200390amarjeet@gmail.com>
2024-04-17 21:21:18 +0530

    Fixed images path(Trial 6)

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
