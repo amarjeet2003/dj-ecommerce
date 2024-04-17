1fc8b3550caec6a28b8acc35b0606c373b275d0c
Amarjeet Singh <200390amarjeet@gmail.com>
2024-04-17 21:32:00 +0530

    Fixed images path(Trial 7)

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ python3.9 -m venv venv
 source venv/bin/activate
 # Install the latest version of pip
 echo "Installing the latest version of pip..."
-python -m pip install-upgrade pip
+python -m pip install --upgrade pip
 # Build the project
 echo "Building the project..."
 python -m pip install -r requirements.txt
